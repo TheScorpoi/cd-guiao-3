c7d32bd7601ba138d8b2c7f2ee121396f3ad7230
TheScorpoi <sobral@ua.pt>
2021-05-04 12:17:17 +0100

    protoc JSON and PICKLE done only broker & mid left

--- src/broker.py
+++ src/broker.py
@@ -1,6 +1,8 @@
 """Message Broker"""
 import enum
 from typing import Dict, List, Any, Tuple
+import selectors
+import socket
 
 class Serializer(enum.Enum):
     """Possible message serializers."""
@@ -18,7 +20,45 @@ class Broker:
         self.canceled = False
         self._host = "localhost"
         self._port = 5000
+        self._sel = selectors.DefaultSelector()
+        self._socket = socket.socket()
+        self._bind(self._host, self._port)
+        self._socket.listen(100)
+        self._sel.register(self._sock, selectors.EVENT_READ, self.accept)
+
+    def accept(self, sock, mask):
+        conn, addr = sock.accept()
+        print('accepted', conn, 'from', addr)
+        conn.setblocking(False)
+        self._sel.register(conn, selectors.EVENT_READ, self.read) 
+        self._connKeyDic[conn] = [None] 
         
+    def read(self,conn, mask):
+        try:
+            data = CDProto.recv_msg(conn) 
+            print('echoing', str(data), 'to', conn)
+            
+            if (data._command == "join" ):
+                channelList = self._connKeyDic[conn]  
+                
+                if data._channel not in channelList:
+                    channelList.append(data._channel)
+                
+                    if(None in channelList):   
+                        channelList.remove(None)
+                    
+                    self._connKeyDic[conn] = channelList    
+
+            if (data._command == "message"):    
+                for k,v in self._connKeyDic.items():    
+                    for channel in v:
+                        if data._channel == channel:    
+                            CDProto.send_msg(k, data)   
+        except ConnectionError:
+            print('closing', conn)
+            self._sel.unregister(conn) 
+            self._connKeyDic.pop(conn)  
+            conn.close()
 
     def list_topics(self) -> List[str]:
         """Returns a list of strings containing all topics."""
--- src/middleware.py
+++ src/middleware.py
@@ -2,7 +2,9 @@
 from collections.abc import Callable
 from enum import Enum
 from queue import LifoQueue, Empty
-
+import json
+import pickle
+import xml
 
 class MiddlewareType(Enum):
     """Middleware Type."""
@@ -16,11 +18,14 @@ class Queue:
 
     def __init__(self, topic, _type=MiddlewareType.CONSUMER):
         """Create Queue."""
+        self._topic = topic
+        self._type = _type
+        self.queue = queue._type(f"{topic}", self._type)
         
-
-
+                
     def push(self, value):
         """Sends data to broker. """
+
 
 
     def pull(self) -> (str, tuple):
@@ -39,13 +44,59 @@ class Queue:
 
 class JSONQueue(Queue):
     """Queue implementation with JSON based serialization."""
-
-
-
+    
+    def __init__(self, topic, _type=MiddlewareType.CONSUMER):
+        super().__init__(topic, _type=_type)
+    
+    @classmethod
+    def encodeJSON(cls, message, topic, method):
+        protocol_JSON = {"method": method, 'topic': topic, "message": message}
+        protocol_JSON = json.dumps(protocol_JSON)
+        protocol_JSON = protocol_JSON.encode('utf-8')
+        return protocol_JSON
+        
+    @classmethod
+    def decodeJSON(cls, data):
+        self.data = data.decode('utf-8')
+        self.data = json.loads(data)
+        method = data['method']
+        topic = data['topic']
+        message = data['message']
+        return method, topic, message
+    
 class XMLQueue(Queue):
     """Queue implementation with XML based serialization."""
-
+    
+    def __init__(self, topic, _type=MiddlewareType.CONSUMER):
+        super().__init__(topic, _type=_type)
+        
+    @classmethod
+    def encodeXML(cls, message, topic, method):
+        protocol_XML = {"method": method, 'topic': topic, "message": message}
+        protocol_XML = {'<?xml version = "1.0" encoding = "UTF-8" standalone = "no" ?>'}
+        protocol_XML = xml.encode('utf-8')
+        return protocol_XML
+        
+    @classmethod
+    def decodeXML(cls, data):
+        return data
 
 class PickleQueue(Queue):
     """Queue implementation with Pickle based serialization."""
+    
+    def __init__(self, topic, _type=MiddlewareType.CONSUMER):
+        super().__init__(topic, _type=_type)
+    
+    @classmethod
+    def encodePICKLE(cls, message, topic, method):
+        protocol_PICKLE = {"method": method, 'topic': topic, "message": message}
+        protocol_PICKLE = pickle.dumps(protocol_PICKLE)
+        return protocol_PICKLE
 
+    @classmethod
+    def decodePICKLE(cls, data):
+        self.data = pickle.loads(data)
+        method = data['method']
+        topic = data['topic']
+        message = data['message']
+        return method, topic, message
