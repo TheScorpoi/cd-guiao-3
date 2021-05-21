ce6359bbf81ba988bcee10ab74f2ecaf0c3f9ec2
TheScorpoi <sobral@ua.pt>
2021-05-21 18:00:45 +0100

    updates middleware

--- src/middleware.py
+++ src/middleware.py
@@ -3,12 +3,14 @@ from collections.abc import Callable
 from enum import Enum
 from queue import LifoQueue, Empty
 from typing import Any
-import pickle
+from broker import Broker
+import socket
+import selectors
 import json
+import pickle
 import xml.etree.ElementTree as element_tree
 import xml
-import selectors
-import socket
+
 
 class MiddlewareType(Enum):
     """Middleware Type."""
@@ -22,58 +24,61 @@ class Queue:
 
     def __init__(self, topic, _type=MiddlewareType.CONSUMER):
         """Create Queue."""
-        self._topic = topic
+        self.topic = topic
         self._type = _type
         self.host = 'localhost'
-        self.port = 5000
+        self.port = 5003 
         self.selector = selectors.DefaultSelector()
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.connect((self.host, self.port))
         self.selector.register(self.socket, selectors.EVENT_READ, self.pull)
-        
-        ack_msg = json.dumps({"method": "ACK_MSG", "Serializer": str(self.__class__.__name__)}).encode('utf-8')
-        
-        self.socket.send(ack_msg)
-        
-        if (self._type == MiddlewareType.CONSUMER):
+        ack_msg = json.dumps({"method": "ACK", "Serializer": str(self.__class__.__name__)}).encode('utf-8')
+        header = len(ack_msg).to_bytes(2, "big")   
+        self.socket.send(header + ack_msg)
+
+        if self._type==MiddlewareType.CONSUMER:
             self.subscribe(topic)
-        
+
+    def subscribe(self, topic):
+        self.send_message('SUBSCRIBE' , topic)
+
     def push(self, value):
         """Sends data to broker. """
+        print(value)
         self.send_message('PUBLISH', value)
 
 
     def pull(self) -> (str, Any):
         """Waits for (topic, data) from broker.
         Should BLOCK the consumer!"""
-        data = self.socket.recv(1000)
         
-        if data:
-            method, topic, message = self.decode(data)
-            return topic, message
-        #else:
-            #return topic, "No data"
+        header = self.socket.recv(2)                        
+        header = int.from_bytes(header, "big")  
+        data = self.socket.recv(header)
+        
+        if data:           
+            method, topic, msg = self.decode(data)   
+            return topic, msg    
 
-    def send_message(self, method, data):
-        data = self.encode(data, self._topic, method)
-        self.socket.send(data)
-        
     def list_topics(self, callback: Callable):
         """Lists all topics available in the broker."""
         self.send_message('LIST', '')
-        
+
     def cancel(self):
         """Cancel subscription."""
-        self.send_message('CANCEL', self._topic)
+        self.send_message('CANCEL', self.topic)
 
-    def subscribe(self, topic):
-        self.send_message('SUBSCRIBE' , topic)
+    def send_message(self, method, message):
+        """Sends through a connection a Message object."""
+        data = self.encode(method, self.topic, message) 
+        header = len(data).to_bytes(2, "big")  
+        self.socket.send(header + data)       
 
 class JSONQueue(Queue):
     """Queue implementation with JSON based serialization."""
     
     def __init__(self, topic, _type=MiddlewareType.CONSUMER):
-        super().__init__(topic, _type=_type)
+        super().__init__(topic, _type)
     
     @classmethod
     def encode(cls, message, topic, method):
@@ -95,7 +100,7 @@ class XMLQueue(Queue):
     """Queue implementation with XML based serialization."""
     
     def __init__(self, topic, _type=MiddlewareType.CONSUMER):
-        super().__init__(topic, _type=_type)
+        super().__init__(topic, _type)
         
     @classmethod
     def encode(cls, message, topic, method):
@@ -118,7 +123,7 @@ class PickleQueue(Queue):
     """Queue implementation with Pickle based serialization."""
     
     def __init__(self, topic, _type=MiddlewareType.CONSUMER):
-        super().__init__(topic, _type=_type)
+        super().__init__(topic, _type)
     
     @classmethod
     def encode(cls, message, topic, method):
